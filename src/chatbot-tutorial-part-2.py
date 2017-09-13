66484eb9bf5f7f48b62be28dca6093cc52d8e3d9
jhancock1975 <jhancock1975@gmail.com>
2017-09-12 20:32:42 -0400

    Update chatbot-tutorial-part-2.py

--- src/chatbot-tutorial-part-2.py
+++ src/chatbot-tutorial-part-2.py
@@ -4,6 +4,7 @@
 # In[1]:
 
 
+# This code is copied from https://chatbotsmagazine.com/contextual-chat-bots-with-tensorflow-4391749d0077 .
 # things we need for NLP
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
